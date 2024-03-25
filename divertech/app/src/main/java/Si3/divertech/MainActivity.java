327b58b255afa3a809796fadb364725abad8791f
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-03-25 18:31:01 +0100

    test pour gradle

---
+++ divertech/app/src/main/java/Si3/divertech/MainActivity.java
@@ -0,0 +1,14 @@
+package Si3.divertech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
