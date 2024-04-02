aa28a64c648f4c92f7fabb72db338cb736730234
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-04-02 16:10:20 +0200

    feat(#5): view fonctionnelle reste ajustement taille

--- divertech/app/src/main/java/Si3/divertech/ChangementLangue.java
+++ divertech/app/src/main/java/Si3/divertech/ChangementLangue.java
@@ -1,7 +1,9 @@
 package Si3.divertech;
 
-import android.content.Context;
+
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,14 @@ public class ChangementLangue extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.changement_langue);
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.langues);
+//create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 }
