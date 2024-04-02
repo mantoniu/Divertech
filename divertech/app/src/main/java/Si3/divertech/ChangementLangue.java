eb407e4805a4947c35155b2e12d43922bdd46303
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-04-02 21:09:40 +0200

    feat(#3): ajout de la liste des langues définies pour la page de création de compte à celle de sélection de la langue

--- divertech/app/src/main/java/Si3/divertech/ChangementLangue.java
+++ divertech/app/src/main/java/Si3/divertech/ChangementLangue.java
@@ -1,10 +1,11 @@
 package Si3.divertech;
 
 
+
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChangementLangue extends AppCompatActivity{
@@ -13,14 +14,11 @@ public class ChangementLangue extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.changement_langue);
-        //get the spinner from the xml.
         Spinner dropdown = findViewById(R.id.langues);
-//create a list of items for the spinner.
-        String[] items = new String[]{"1", "2", "three"};
-//create an adapter to describe how the items are displayed, adapters are used in several places in android.
-//There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-//set the spinners adapter to the previously created one.
+        String[] languages = getResources().getStringArray(R.array.languages);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, languages);
         dropdown.setAdapter(adapter);
+        View b = findViewById(R.id.returnArrow);
+        b.setOnClickListener(click->finish());
     }
 }
