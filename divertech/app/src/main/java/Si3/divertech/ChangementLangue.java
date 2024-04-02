6c2baaeb753d77bd3af3f7519e1477e3305ade6d
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-04-02 14:52:53 +0200

    feat(#3) : ajout du début de la vue de changement de langue

---
+++ divertech/app/src/main/java/Si3/divertech/ChangementLangue.java
@@ -0,0 +1,15 @@
+package Si3.divertech;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangementLangue extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.changement_langue);
+    }
+}
