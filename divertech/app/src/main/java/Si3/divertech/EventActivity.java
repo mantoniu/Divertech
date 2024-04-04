13101459244d0a311a1727886ecc5052594735a6
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-04-04 23:48:35 +0200

    feat(#7): ajout d'une première version des vues

--- divertech/app/src/main/java/Si3/divertech/EventActivity.java
+++ divertech/app/src/main/java/Si3/divertech/EventActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 
 public class EventActivity extends AppCompatActivity {
 
+
+    public static final int REPORTING = 0;
+    public static final int CONTACT = 1;
+    public static final int OBJET = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
