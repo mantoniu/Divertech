89a9a3c6f9fd32e82b92e1e60dff753a0cfb1bc0
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-04-10 23:14:27 +0200

    feat(#26): add map to the MapActivity

--- divertech/app/src/main/java/Si3/divertech/MapActivity.java
+++ divertech/app/src/main/java/Si3/divertech/MapActivity.java
@@ -3,9 +3,19 @@ package Si3.divertech;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity implements ClickableActivity{
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements ClickableActivity, OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +27,34 @@ public class MapActivity extends AppCompatActivity implements ClickableActivity{
         FootMenu f = new FootMenu();
         f.setArguments(b);
         getSupportFragmentManager().beginTransaction().add(R.id.footMenu,f).commit();
+
+
+        MapView mapFragment = findViewById(R.id.map);
+        mapFragment.onCreate(new Bundle());
+        mapFragment.getMapAsync(this);
+
     }
+
 
     @Override
     public Context getContext(){
         return getApplicationContext();
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
+
+
+
+
+
