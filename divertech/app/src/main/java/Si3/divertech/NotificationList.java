4ccb13b84b72ed507bda63e547467854b8e96ccd
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2024-04-23 14:52:33 +0200

    feat(#29): Modification de la popup

--- divertech/app/src/main/java/Si3/divertech/NotificationList.java
+++ divertech/app/src/main/java/Si3/divertech/NotificationList.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NotificationList {
-    private static final Notification notification1 = new Notification("0", "0", ListEvent.getEvent("0").getPictureUrl(), NotificationTypes.INCIDENT, "Un incident à eu lieu");
+    private static final Notification notification1 = new Notification("0", "0", ListEvent.getEvent("0").getPictureUrl(), NotificationTypes.INCIDENT, "Un incident à eu lieu Iff eziohgio zogi ezrjg er gjozr kgj jker gkjer kjgenropgj eripjg aer ngkmja erkjg jerngiovreoing jnaekjg kea roibgerioahbio nake nrklgaek rijeraiphj ipearjio hklr  realn giernaiph khrn, klhnaerjl nhineri nhlkenlknlmenrioag jiera igjiera ngkndjkl nglarneing opaerohg pag piarnegn aekrn gkearngkl ernih iernklg nfnklg nerkln hklrnemh arnehl aerkhnaekl nhklnrekl anh");
     private static final Notification notification2 = new Notification("1", "1", ListEvent.getEvent("1").getPictureUrl(), NotificationTypes.INCIDENT, "Un incident à eu lieu");
     private static final Notification notification3 = new Notification("2", "2", ListEvent.getEvent("2").getPictureUrl(), NotificationTypes.INCIDENT, "Un incident à eu lieu");
     private static final Notification notification4 = new Notification("3", "3", ListEvent.getEvent("3").getPictureUrl(), NotificationTypes.INCIDENT, "Un incident à eu lieu");
