6098d32dd5a1eaee0a4b1d439626bc1b9aa01858
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2024-05-19 18:05:27 +0200

    feat(#40): Ajout popup qr code

--- divertech/app/build.gradle.kts
+++ divertech/app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 dependencies {
     implementation("com.google.android.gms:play-services-location:21.2.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation("com.github.androidmads:QRGenerator:1.0.1")
     implementation("io.github.elye:loaderviewlibrary:3.0.0")
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.vanniktech:android-image-cropper:4.5.0")
--- divertech/app/src/main/java/Si3/divertech/events/AdminEventActivity.java
+++ divertech/app/src/main/java/Si3/divertech/events/AdminEventActivity.java
@@ -1,13 +1,26 @@
 package Si3.divertech.events;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import Si3.divertech.CreateEventActivity;
 import Si3.divertech.MainActivity;
 import Si3.divertech.MultiPagesActivity;
 import Si3.divertech.R;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class AdminEventActivity extends EventActivities {
     @Override
@@ -21,13 +34,45 @@ public class AdminEventActivity extends EventActivities {
         View reportButton = findViewById(R.id.button_report);
         reportButton.setOnClickListener(click -> startActivity(report));
 
-        View feed = findViewById(R.id.card_feed_type);
-        feed.setOnClickListener(click -> {
+        findViewById(R.id.share).setOnClickListener(click -> createPopup());
+
+        findViewById(R.id.card_feed_type).setOnClickListener(click -> {
             Log.d("Admin", getEventId() + " ");
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             intent.putExtra(getString(R.string.event_id), getEventId());
             intent.putExtra(getString(R.string.back_possible), true);
             startActivity(intent);
         });
+    }
+
+
+    private void createPopup() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.share_popup, null);
+
+        int width = LinearLayout.LayoutParams.MATCH_PARENT;
+        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+
+        PopupWindow popup = new PopupWindow(popupView, width, height, true);
+
+        popup.setAnimationStyle(R.style.Animation);
+
+        TextView eventId = popupView.findViewById(R.id.event_id);
+        eventId.setText(getEventId());
+        eventId.setOnClickListener(click -> {
+            ClipboardManager cm = (ClipboardManager) getApplicationContext().getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("Event ID", getEventId());
+            cm.setPrimaryClip(clip);
+            Toast.makeText(getApplicationContext(), "Copié dans le presse papier", Toast.LENGTH_SHORT).show();
+        });
+
+        QRGEncoder qrgEncoder = new QRGEncoder(getEventId(), null, QRGContents.Type.TEXT, 200);
+        Bitmap bitmap = qrgEncoder.getBitmap(0);
+        ((ImageView) popupView.findViewById(R.id.qr_code)).setImageBitmap(bitmap);
+
+        popupView.findViewById(R.id.layout).setOnClickListener((click) -> popup.dismiss());
+        popupView.findViewById(R.id.close_button).setOnClickListener((click) -> popup.dismiss());
+
+        popup.showAtLocation(getWindow().getDecorView(), Gravity.CENTER, 0, 0);
     }
 }
