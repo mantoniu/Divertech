87b2ebcf144adb66a1e244ab058d50c98b13ecde
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-05-24 23:59:13 +0200

    feat(#60): ajout de l'image de profil dans la bar d'action (barre en haut) et retravailler les menus pour que tout fonctionne bien

--- divertech/app/src/main/java/Si3/divertech/HeaderMenu.java
+++ divertech/app/src/main/java/Si3/divertech/HeaderMenu.java
@@ -1,6 +1,5 @@
 package Si3.divertech;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,19 +9,26 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import Si3.divertech.users.User;
+import com.squareup.picasso.Picasso;
+
+import java.util.Observable;
+import java.util.Observer;
+
 import Si3.divertech.users.UserData;
 
-public class HeaderMenu extends Fragment {
-    private Activity activity;
+public class HeaderMenu extends Fragment implements Observer {
+
+    private Menu menu = null;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        UserData.getInstance().addObserver(this);
         setHasOptionsMenu(true);
         super.onCreate(savedInstanceState);
     }
@@ -38,32 +44,69 @@ public class HeaderMenu extends Fragment {
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.header_menu, menu);
         super.onCreateOptionsMenu(menu, inflater);
+        if (this.menu == null) {
+            this.menu = menu;
+            updateToolbar();
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        //handle menu item clicks
-        int id = item.getItemId();
-        User user = UserData.getInstance().getConnectedUser();
-        if (id == R.id.menu_language_english) {
-            user.setLanguage("en");
-        } else if (id == R.id.menu_language_french) {
-            user.setLanguage("fr");
-        } else if (id == R.id.menu_logout) {
-            Intent intent = new Intent(getActivity(), LoginActivity.class);
-            user.disconnect();
-            startActivity(intent);
-            activity.finish();
-        }
-        else if(id == R.id.menu_profile_edit){
-            Intent intent = new Intent(getActivity(), ProfileActivity.class);
-            startActivity(intent);
-        }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+    }
+
+    public void updateToolbar() {
+        MenuItem profileImageItem = menu.findItem(R.id.menu_profile);
+        View profileImageView = profileImageItem.getActionView();
+        if (profileImageView != null)
+            profileImageView.setOnClickListener(this::showProfilePopupMenu);
+        Picasso.get().load(UserData.getInstance().getConnectedUser().getPictureUrl()).into((android.widget.ImageView) profileImageView.findViewById(R.id.profile_picture));
+        MenuItem langImage = menu.findItem(R.id.menu_language);
+        if (langImage.getActionView() != null)
+            langImage.getActionView().setOnClickListener(this::showLangMenu);
+    }
+
+    private void showProfilePopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(getContext(), view);
+        popupMenu.inflate(R.menu.profile_popup_menu);
+        popupMenu.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menu_logout) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                UserData.getInstance().getConnectedUser().disconnect();
+                startActivity(intent);
+            } else if (id == R.id.menu_profile_edit) {
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(intent);
+            }
+            return false;
+        });
+        popupMenu.show();
+    }
+
+    private void showLangMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(getContext(), view);
+        popupMenu.inflate(R.menu.lang_menu);
+        popupMenu.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menu_language_english) {
+                UserData.getInstance().getConnectedUser().setLanguage("en");
+            } else if (id == R.id.menu_language_french) {
+                UserData.getInstance().getConnectedUser().setLanguage("fr");
+            }
+            return false;
+        });
+        popupMenu.show();
+    }
+
+
+    @Override
+    public void update(Observable o, Object arg) {
+        updateToolbar();
     }
 }
