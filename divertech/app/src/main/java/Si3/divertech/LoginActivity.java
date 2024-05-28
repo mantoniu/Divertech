a7f2078add200aebee54125074b746444022dc20
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-05-28 23:41:02 +0200

    feat(#69) : remove google connection and correct layout event activity

--- divertech/app/src/main/java/Si3/divertech/LoginActivity.java
+++ divertech/app/src/main/java/Si3/divertech/LoginActivity.java
@@ -6,7 +6,6 @@ import android.text.Layout;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.AlignmentSpan;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -15,16 +14,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -33,9 +26,7 @@ import Si3.divertech.databinding.ActivityLoginBinding;
 import Si3.divertech.users.UserData;
 
 public class LoginActivity extends AppCompatActivity implements Observer {
-    private static final int RC_SIGN_IN = 123;
     private FirebaseAuth mAuth;
-    private GoogleSignInClient mGoogleSignInClient;
     private ActivityLoginBinding binding;
 
     @Override
@@ -108,49 +99,6 @@ public class LoginActivity extends AppCompatActivity implements Observer {
                         }
                     });
         });
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        Button google = findViewById(R.id.google);
-        google.setOnClickListener(click -> signInGoogle());
-    }
-
-
-    private void signInGoogle() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            try {
-                GoogleSignInAccount account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException.class);
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-                Log.d("LOGIN ERROR", "", e);
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(String idToken) {
-        mAuth.signInWithCredential(GoogleAuthProvider.getCredential(idToken, null))
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(this, "Connexion réussie", Toast.LENGTH_SHORT).show();
-                        Intent i = new Intent(this, MainActivity.class);
-                        startActivity(i);
-                        finish();
-                    } else {
-                        Toast.makeText(this, "Impossible de se connecter avec Google :(", Toast.LENGTH_SHORT).show();
-                    }
-                });
     }
 
     @Override
