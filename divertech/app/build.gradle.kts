d54b6e68ace52b49514a4f6d96150deb04f7b340
Heschung Erwan <erwan.heschung@etu.univ-cotedazur.fr>
2024-06-01 19:20:30 +0200

    feat: notifications fonctionnelles #64

--- divertech/app/build.gradle.kts
+++ divertech/app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    implementation("com.google.auth:google-auth-library-oauth2-http:1.17.0")
-    implementation("com.google.firebase:firebase-messaging:23.0.8")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.11.0")
+    implementation("com.google.firebase:firebase-messaging:24.0.0")
     implementation("com.google.android.gms:play-services-location:21.2.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation("com.github.androidmads:QRGenerator:1.0.1")
